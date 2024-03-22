64a5a6abdacbc00af1ab58c93d9b735f62be124b
MatteyGG <matgaranin@gmail.com>
2024-03-22 21:13:04 +0300

    add username

--- app/registration/page.js
+++ app/registration/page.js
@@ -6,13 +6,14 @@ import HeaderPage from "../components/headerComp";
 export default function RegistrationPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const router = useRouter();
   const handleFormSubmit = async function (event) {
     event.preventDefault();
     console.log({ email, password });
     const response = await fetch("/api/registration", {
       method: "POST",
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({username, email, password }),
     });
     headers: {
       "Content-Type", "application/json";
@@ -29,6 +30,10 @@ export default function RegistrationPage() {
     <section className="mt-8 lg:mx-40">
       <HeaderPage text="Регистрация" />
       <form onSubmit={handleFormSubmit}>
+        <label>
+          <span>Почта</span>
+          <input type="text" name="name" placeholder="Иван" value={username} onChange={ev => setUsername(ev.target.value)}/>
+        </label>
         <label>
           <span>Почта</span>
           <input type="email" name="email" placeholder="podgoroy@example.com" value={email} onChange={ev => setEmail(ev.target.value)}/>
